66e3a34cbec439f2b15a29dbbc60dfeb14b6d936
@mohammad_matin_parvin <mohammad123matin@gmail.com>
2021-01-03 20:19:40 +0330

    SDL_Image library added and window created

--- main.c
+++ main.c
@@ -1,15 +1,74 @@
 #include <stdio.h>
-#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
 #include "objects/pacman.h"
 
+//functions prototype
+bool app_init();
+
+//the name of our app
+const char gAppName[] = "PAC-MAN";
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//The window we'll be rendering to
+SDL_Window *gWindow = NULL;
+
+//The window renderer
+SDL_Renderer *gRenderer = NULL;
+
 int main(int argc, char *argv[]) {
+    if (!app_init()) {
+        printf("Failed to initialize the app!\n");
+        return 1;
+    }
+    printf("we create window");
     PACMAN pacMan;
     PACMAN_init(&pacMan, 10, 10);
-    SDL_Init(SDL_INIT_VIDEO);
-    printf("we are going to create Pac-Man!\n");
-    printf("now we have our pacman with width = %d; height = %d; velocity = %d; posX = %d; posY = %d", pacMan.pWidth,
-           pacMan.pHeight, pacMan.pVelocity,
-           pacMan.pPosy, pacMan.pPosx);
-
     return 0;
+}
+//this function check lib, init SDL and SDL_Image, create renderer and window
+bool app_init() {
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        success = false;
+    } else {
+        //Set texture filtering to linear
+        if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+            printf("Warning: Linear texture filtering not enabled!");
+        }
+
+        //Create window
+        gWindow = SDL_CreateWindow(gAppName, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                   SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL) {
+            printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+            success = false;
+        } else {
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+            if (gRenderer == NULL) {
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+                success = false;
+            } else {
+                //Initialize renderer color
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags)) {
+                    printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+                    success = false;
+                }
+            }
+        }
+    }
+    return success;
 }
