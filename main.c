2cfc2f08895dbaf343c9ee3d5576252f9cac8102
@mohammad_matin_parvin <mohammad123matin@gmail.com>
2021-01-03 10:37:31 +0330

    SDL2 library added

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
     printf("we are going to create Pac-Man!\n");
     return 0;
 }
